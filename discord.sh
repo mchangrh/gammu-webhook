a6114add0516f00afaae4a3bc6a0a1dc3713d1af
Michael C <michael@mchang.name>
2023-01-16 20:10:16 -0500

    initial commit

---
+++ discord.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source env.sh
+AREACODE_REGEX='^\+1\d+'
+
+if [[ "$SMS_1_NUMBER" =~ $AREACODE_REGEX ]]
+then
+  AUTHOR="${SMS_1_NUMBER:0:2} ${SMS_1_NUMBER:2:3} ${SMS_1_NUMBER:5:3} ${SMS_1_NUMBER:8:4}"
+else
+  AUTHOR=SMS_1_NUMBER
+fi
+
+curl \
+  -H "Content-Type: application/json" \
+  -d "{\"username\": \"${AUTHOR}\", \"content\": \"${SMS_1_TEXT}\"}" \
+  "$WEBHOOK_URL"
